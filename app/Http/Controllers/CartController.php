e954caab31b1d5ea88bc01d2976e7e934228f3ad
utkarshShukla <shukla@phantasm.co.in>
2024-07-27 13:03:26 +0530

    checkout empty cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -697,6 +697,7 @@ class CartController extends Controller
         }
 
         Cart::where('user_id', $user_id)->delete();
+        $checkout->delete();
 
         return response()->json(['message' => 'All items removed', 'cart_count' => 0, 'status' => true], 200);
     }
