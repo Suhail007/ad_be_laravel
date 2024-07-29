33a0b2f5410bb012b573d8cde532a656d7e1834e
utkarshShukla <shukla@phantasm.co.in>
2024-07-29 10:46:33 +0530

    order stock issue

--- app/Http/Controllers/PayPalController.php
+++ app/Http/Controllers/PayPalController.php
@@ -545,19 +545,19 @@ class PayPalController extends Controller
                         ]);
 
 
-                        if ($item['variation_id']) {
-                            $productMeta = ProductMeta::where('post_id', $item['variation_id'])->where('meta_key', '_stock')->first();
-                            if ($productMeta) {
-                                $productMeta->meta_value -= $item['quantity'];
-                                $productMeta->save();
-                            }
-                        } else {
-                            $productMeta = ProductMeta::where('post_id', $item['product_id'])->where('meta_key', '_stock')->first();
-                            if ($productMeta) {
-                                $productMeta->meta_value -= $item['quantity'];
-                                $productMeta->save();
-                            }
-                        }
+                        // if ($item['variation_id']) {
+                        //     $productMeta = ProductMeta::where('post_id', $item['variation_id'])->where('meta_key', '_stock')->first();
+                        //     if ($productMeta) {
+                        //         $productMeta->meta_value -= $item['quantity'];
+                        //         $productMeta->save();
+                        //     }
+                        // } else {
+                        //     $productMeta = ProductMeta::where('post_id', $item['product_id'])->where('meta_key', '_stock')->first();
+                        //     if ($productMeta) {
+                        //         $productMeta->meta_value -= $item['quantity'];
+                        //         $productMeta->save();
+                        //     }
+                        // }
                         Cart::where('user_id', $user->ID)
                             ->where('product_id', $item['product_id'])
                             ->where('variation_id', $item['variation_id'] ?? null)
