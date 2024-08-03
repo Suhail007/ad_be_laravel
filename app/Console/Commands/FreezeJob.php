6b5ccd71e244b248ebfc2c566a75ad3c051c11ba
utkarshShukla <shukla@phantasm.co.in>
2024-08-03 11:55:46 +0530

    buffer

--- app/Console/Commands/FreezeJob.php
+++ app/Console/Commands/FreezeJob.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class FreezeJob extends Command
@@ -29,6 +30,34 @@ class FreezeJob extends Command
     {
         Log::info('Utkarsh Jobs');
         Artisan::call('queue:work');
+        Artisan::call('schedule:run');
+        $buffers = DB::table('buffers')->get();
+
+        foreach ($buffers as $buffer) {
+            $orderItem = DB::table('wp_woocommerce_order_items')
+                ->where('order_id', $buffer->order_id)
+                ->where('order_item_name', $buffer->shipping)
+                ->first();
+
+            if ($orderItem) {
+                $orderShipping = DB::table('wp_postmeta')
+                    ->where('post_id', $buffer->order_id)
+                    ->where('meta_key', '_order_shipping')
+                    ->value('meta_value');
+
+                if ($orderShipping === '0') {
+                    DB::table('wp_postmeta')
+                        ->where('post_id', $buffer->order_id)
+                        ->where('meta_key', '_order_shipping')
+                        ->update(['meta_value' => '15']);
+
+                    DB::table('buffers')
+                        ->where('id', $buffer->id)
+                        ->delete();
+                    Log::info($buffer->order_id.' shipping charges updated');
+                }
+            }
+        }
       
     }
 }
