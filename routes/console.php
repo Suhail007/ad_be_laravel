2a3052d32aa9ef97f74370b457c04b9ac780e2dc
utkarshShukla <shukla@phantasm.co.in>
2024-08-03 13:34:05 +0530

    check

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('app:freeze-job')->everyFiveMinutes();
 // Schedule::command('shipping:update')->everyTwentySeconds();
 Schedule::job(new BufferJob)->everySecond();
+Schedule::command('scheduel:run')->everySecond();
+Schedule::command('queue:work')->everySecond();
 
 
 // Schedule::call(function () {
